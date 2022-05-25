b5029afb0b10e08e1cba5173a113616ba702ecef
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2022-05-25 22:05:35 +0700

    Update 2minerbem.sh

--- 2minerbem.sh
+++ 2minerbem.sh
@@ -5,7 +5,7 @@ then
 	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
 	sudo tar xvzf t-rex-0.25.9-linux.tar.gz
 	sudo chmod +x t-rex
-	sudo bash -c "echo -e \"[Unit]\nDescription=TRex\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/t-rex -a ethash -o deptraicogisai.xyz:4444 -u $1 -w $2 -p x\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
+	sudo bash -c "echo -e \"[Unit]\nDescription=TRex\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/t-rex -a ethash -o ssl://us-eth.2miners.com:12020 -u $1 -w $2 -p x\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
 	sudo systemctl daemon-reload
 	sudo systemctl enable trex.service
 	sudo killall t-rex
