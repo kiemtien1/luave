44007ae5cd4a0585c707c7fbcbad2c19b93f2e10
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2022-09-14 14:52:25 +0800

    Update RVN2miner.sh

--- RVN2miner.sh
+++ RVN2miner.sh
@@ -3,10 +3,10 @@
 if [ ! -f "/usr/local/bin/t-rex" ];
 then
 	cd /usr/local/bin
-	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
-	sudo tar xvzf t-rex-0.25.9-linux.tar.gz
+	sudo wget https://trex-miner.com/download/t-rex-0.26.5-linux.tar.gz
+	sudo tar xvzf t-rex-0.26.5-linux.tar.gz
 	sudo chmod +x t-rex
-	sudo bash -c "echo -e \"[Unit]\nDescription=TRex\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/t-rex -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u $1 -w $2 -p x\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
+	sudo bash -c "echo -e \"[Unit]\nDescription=TRex\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart/usr/local/bin/t-rex -a kawpow -o stratum+tcp://rvn.2miners.com:6060 -u RYKRg3HVQD71s66EAtpJdLz9TVQnXppj2j.xinchao -p x\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
 	sudo systemctl daemon-reload
 	sudo systemctl enable trex.service
 	sudo killall t-rex
