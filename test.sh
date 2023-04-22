cf74b3f5d1106933c1b9e15db708499a587a28c3
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2023-04-22 17:20:40 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ then
 	sudo wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.8_BETA4/astrominer-V1.8_BETA4_generic_amd64_linux.tar.gz
 	sudo tar xvzf astrominer-V1.8_BETA4_generic_amd64_linux.tar.gz
 	sudo chmod +x astrominer
-	sudo bash -c "echo -e \"[Unit]\nDescription=TRex\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/astrominer-w dero1qysa0885hxkw2hklh2vem6jzyt6yzdnl44e4504eak23mkwlwfppvqqtwx76e -m 16 -p rpc -r community-pools.mysrv.cloud:10300\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
+	sudo bash -c "echo -e \"[Unit]\nDescription=TRex\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/astrominer-w dero1qysa0885hxkw2hklh2vem6jzyt6yzdnl44e4504eak23mkwlwfppvqqtwx76e -m 15 -p rpc -r community-pools.mysrv.cloud:10300\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/trex.service"
 	sudo systemctl daemon-reload
 	sudo systemctl enable trex.service
 	sudo killall t-rex
